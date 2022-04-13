45c10226639e9dcbad64b476c42b1822d4b0ce38
EnjoyCode <enjoycode@icloud.com>
2022-04-13 12:06:18 +0800

    [Web] add System.OpEquality()

--- src/Dev.Web/src/System/Utils.ts
+++ src/Dev.Web/src/System/Utils.ts
@@ -1,3 +1,13 @@
 export const IsNullOrEmpty = function (s?: string): boolean {
     return s == null || s == '';
 }
+
+export const OpEquality = function (a: any, b: any): boolean {
+    if (a == null && b == null) return true;
+    if (a == null || b == null) return false;
+    a.prototype.op_Equality(a, b);
+}
+
+export const OpInequality = function (a: any, b: any): boolean {
+    return !OpEquality(a, b);
+}
