21308be82163a86825e1b27de5f44485913c9c70
EnjoyCode <enjoycode@icloud.com>
2022-04-14 10:12:07 +0800

    [CS2TS] 处理Array.Empty()

--- src/Dev.Web/tests/unit/EqualityTest.ts
+++ src/Dev.Web/tests/unit/EqualityTest.ts
@@ -7,6 +7,8 @@ describe("Equality tests", () => {
         let a = new Point(1, 2);
         let b = new Point(1, 2);
 
+        //let ds = new Map<Point, string>([[a, "1"], [b, "2"]]);
+
         let res = OpEquality(a, b);
         expect(res).toEqual(true);
     });
