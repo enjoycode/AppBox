91c80eb37b17772fcc42b97815714e124f268f43
EnjoyCode <enjoycode@icloud.com>
2022-04-20 13:13:37 +0800

    [Widgets] impl EditableText.IsObscure property.

--- src/Dev/LoginPage.cs
+++ src/Dev/LoginPage.cs
@@ -4,6 +4,27 @@ namespace AppBoxDev
 {
     public sealed class LoginPage: View
     {
-        
+        public LoginPage()
+        {
+            Child = new Center
+            {
+                Child = new Card
+                {
+                    Width = 400,
+                    Height = 500,
+                }
+            };
+        }
+
+        private Widget BuildForm()
+        {
+            return new Column
+            {
+                Children = new Widget[]
+                {
+                    new Text("Welcome") {FontSize = 50},
+                }
+            };
+        }
     }
 }
