b37dc6f74f95187b4bf49214007dcc0bff9c7af2
EnjoyCode <enjoycode@icloud.com>
2022-04-21 14:55:32 +0800

    [Web] 防止事件激发过程中移除事件监听

--- src/Dev.Web/src/System/Event.ts
+++ src/Dev.Web/src/System/Event.ts
@@ -5,6 +5,7 @@ interface IListener<T> {
 
 export class Event<T = void> {
     private _listeners?: IListener<T>[];
+    private _it: number = -1;
 
     public Add(listener: (arg: T) => void, caller?: any) {
         if (!this._listeners)
@@ -25,22 +26,29 @@ export class Event<T = void> {
                 break;
             }
         }
+
+        if (this._it >= 0) { //正在激发过程中移除自己
+            this._it--;
+        }
     }
 
     public Invoke(arg: T) {
         if (!this._listeners) return;
-        for (let i = 0; i < this._listeners.length; i++) {
-            const item = this._listeners[i];
+
+        this._it = 0;
+        while (this._it < this._listeners.length) {
+            const item = this._listeners[this._it];
             const target = item.target?.deref();
             const notAlive = item.target !== undefined && target === undefined;
 
             if (notAlive) {
-                this._listeners.splice(i, 1);
-                i--;
+                this._listeners.splice(this._it, 1);
             } else {
                 item.callback.call(target, arg);
+                this._it++;
             }
         }
+        this._it = -1;
     }
 
 }
