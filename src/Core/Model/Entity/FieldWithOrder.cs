7a72247518e4e614623e88a0e435723f0fdd8db3
EnjoyCode <enjoycode@icloud.com>
2022-07-14 16:34:13 +0800

    [Core] FieldWithOrder手工序列化.

--- src/Core/Model/Entity/FieldWithOrder.cs
+++ src/Core/Model/Entity/FieldWithOrder.cs
@@ -3,15 +3,26 @@ namespace AppBoxCore;
 /// <summary>
 /// 带排序标志的实体成员标识,用于主键或索引等
 /// </summary>
-[BinSerializable(BinSerializePolicy.Compact)]
-public partial struct FieldWithOrder
+public struct FieldWithOrder : IBinSerializable
 {
-    [Field(1)] public short MemberId { get; private set; }
-    [Field(2)] public bool OrderByDesc { get; private set; }
+    public short MemberId { get; private set; }
+    public bool OrderByDesc { get; private set; }
 
     public FieldWithOrder(short memberId, bool orderByDesc = false)
     {
         MemberId = memberId;
         OrderByDesc = orderByDesc;
+    }
+
+    public void WriteTo(IOutputStream ws)
+    {
+        ws.WriteShort(MemberId);
+        ws.WriteBool(OrderByDesc);
+    }
+
+    public void ReadFrom(IInputStream rs)
+    {
+        MemberId = rs.ReadShort();
+        OrderByDesc = rs.ReadBool();
     }
 }
