4d2f40d8b176579dd23937957c9ee16b757de2af
EnjoyCode <enjoycode@icloud.com>
2022-11-24 19:39:44 +0800

    [PIxUI] format code.

--- src/PixUI/PixUI/Widgets/Menu/MenuController.cs
+++ src/PixUI/PixUI/Widgets/Menu/MenuController.cs
@@ -23,36 +23,35 @@ namespace PixUI
 
         internal void OnMenuItemHoverChanged(MenuItemWidget item, bool hover)
         {
-            if (hover)
+            if (!hover) return;
+
+            //尝试关闭之前打开的
+            if (_popupMenuStack != null && _popupMenuStack.TryCloseSome(item))
+                return;
+
+            //尝试打开子菜单
+            if (item.MenuItem.Type == MenuItemType.SubMenu)
             {
-                //尝试关闭之前打开的
-                if (_popupMenuStack != null && _popupMenuStack.TryCloseSome(item))
-                    return;
+                _popupMenuStack ??= new PopupMenuStack(item.Overlay!, CloseAll);
 
-                //尝试打开子菜单
-                if (item.MenuItem.Type == MenuItemType.SubMenu)
-                {
-                    _popupMenuStack ??= new PopupMenuStack(item.Overlay!, CloseAll);
+                var popupMenu = new PopupMenu(item, null, item.Depth + 1, this);
+                var relativeToWinPt = item.LocalToWindow(0, 0);
+                // var relativeBounds =
+                //     Rect.FromLTWH(relativeToWinPt.X, relativeToWinPt.Y, item.W, item.H);
+                //TODO:计算弹出位置
+                if (item.Parent is PopupMenu)
+                    popupMenu.SetPosition(relativeToWinPt.X + item.W, relativeToWinPt.Y);
+                else
+                    popupMenu.SetPosition(relativeToWinPt.X, relativeToWinPt.Y + item.H);
+                var win = item.Root!.Window;
+                popupMenu.Layout(win.Width, win.Height);
 
-                    var popupMenu = new PopupMenu(item, null, item.Depth + 1, this);
-                    var relativeToWinPt = item.LocalToWindow(0, 0);
-                    // var relativeBounds =
-                    //     Rect.FromLTWH(relativeToWinPt.X, relativeToWinPt.Y, item.W, item.H);
-                    //TODO:计算弹出位置
-                    if (item.Parent is PopupMenu)
-                        popupMenu.SetPosition(relativeToWinPt.X + item.W, relativeToWinPt.Y);
-                    else
-                        popupMenu.SetPosition(relativeToWinPt.X, relativeToWinPt.Y + item.H);
-                    var win = item.Root!.Window;
-                    popupMenu.Layout(win.Width, win.Height);
-
-                    _popupMenuStack.Add(popupMenu);
-                }
-
-                //如果没有打开的子菜单，移除整个PopupStack
-                if (_popupMenuStack != null && !_popupMenuStack.HasChild)
-                    CloseAll();
+                _popupMenuStack.Add(popupMenu);
             }
+
+            //如果没有打开的子菜单，移除整个PopupStack
+            if (_popupMenuStack != null && !_popupMenuStack.HasChild)
+                CloseAll();
         }
 
         internal void ShowContextMenu(MenuItem[] menuItems)
