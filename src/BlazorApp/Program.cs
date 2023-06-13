a20b07f035195375a9dcf10ec349c4d7ad5baf35
EnjoyCode <enjoycode@icloud.com>
2023-06-13 09:37:48 +0800

    [Blazor] 使用sys.HomePage作为启动页面.

--- src/BlazorApp/Program.cs
+++ src/BlazorApp/Program.cs
@@ -6,7 +6,7 @@ using PixUI.Platform.Blazor;
 
 namespace AppBox.BlazorApp;
 
-public class Program
+public static class Program
 {
     public static async Task Main(string[] args)
     {
@@ -16,22 +16,31 @@ public class Program
         var host = builder.Build();
         BlazorApplication.JSRuntime = host.Services.GetRequiredService<IJSRuntime>();
         BlazorApplication.HttpClient = host.Services.GetService<HttpClient>()!;
-        
+
         await host.RunAsync();
     }
-    
+
     [JSInvokable]
     public static async Task Run(int glHandle, int width, int height, float ratio, string? routePath)
     {
         //初始化通讯
         Channel.Init(new WebSocketChannel(new Uri("ws://localhost:5137/ws"))); //TODO: fix uri
-        
+
         //初始化默认字体
         await using var fontDataStream =
             await BlazorApplication.HttpClient.GetStreamAsync("/fonts/MiSans-Regular.woff2");
         using var fontData = SKData.Create(fontDataStream);
         FontCollection.Instance.RegisterTypefaceToAsset(fontData!, FontCollection.DefaultFamilyName, false);
-        
-        BlazorApplication.Run(() => new IndexPage(), glHandle, width, height, ratio, routePath);
+
+        //加载HomePage
+        var homePage = await AppAssembiles.MakeViewWidgetAsync("sys.HomePage");
+        // ReSharper disable once SuspiciousTypeConversion.Global
+        if (homePage is IHomePage mainRoutes)
+        {
+            var devRoute = new Route("dev", s => new AppBoxDesign.HomePage());
+            mainRoutes.InjectRoute(devRoute);
+        }
+
+        BlazorApplication.Run(() => homePage, glHandle, width, height, ratio, routePath);
     }
 }
