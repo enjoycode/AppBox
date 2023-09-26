355f0cf29efaca6c863783a2aa0528333c03c909
EnjoyCode <enjoycode@icloud.com>
2023-09-26 11:13:16 +0800

    [Dynamic] short path for register.

--- src/Client.UI/Dynamic/DynamicInitiator.cs
+++ src/Client.UI/Dynamic/DynamicInitiator.cs
@@ -10,25 +10,23 @@ public sealed class DynamicInitiator
 {
     static DynamicInitiator()
     {
-        DynamicWidgetManager.Register(
-            DynamicWidgetMeta.Make<DynamicCartesianChart>(MaterialIcons.BarChart,
-                catalog: "Chart",
-                name: "CartesianChart",
-                properties: new DynamicPropertyMeta[]
-                {
-                    new("Series", typeof(CartesianSeriesSettings[]), true),
-                    new("XAxes", typeof(AxisSettings[]), true),
-                    new("YAxes", typeof(AxisSettings[]), true)
-                }));
-        DynamicWidgetManager.Register(
-            DynamicWidgetMeta.Make<DynamicPieChart>(MaterialIcons.PieChart,
-                catalog: "Chart",
-                name: "PieChart",
-                properties: new DynamicPropertyMeta[]
-                {
-                    new("Series", typeof(PieSeriesSettings), true),
-                    new("LegendPosition", typeof(LegendPosition), false),
-                }));
+        DynamicWidgetManager.Register<DynamicCartesianChart>(MaterialIcons.BarChart,
+            catalog: "Chart",
+            name: "CartesianChart",
+            properties: new DynamicPropertyMeta[]
+            {
+                new("Series", typeof(CartesianSeriesSettings[]), true),
+                new("XAxes", typeof(AxisSettings[]), true),
+                new("YAxes", typeof(AxisSettings[]), true)
+            });
+        DynamicWidgetManager.Register<DynamicPieChart>(MaterialIcons.PieChart,
+            catalog: "Chart",
+            name: "PieChart",
+            properties: new DynamicPropertyMeta[]
+            {
+                new("Series", typeof(PieSeriesSettings), true),
+                new("LegendPosition", typeof(LegendPosition), false),
+            });
     }
 
     public static void TryInit() { }
