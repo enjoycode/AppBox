403c1ac237ddbe49c474267dc3ddd43cff398fff
EnjoyCode <enjoycode@icloud.com>
2023-10-27 08:50:54 +0800

    [Design]给LoginPage加个背景.

--- src/AppStudio/LoginPage.cs
+++ src/AppStudio/LoginPage.cs
@@ -1,71 +1,81 @@
 using System;
 using AppBoxClient;
+using AppBoxDesign.Resources;
 using PixUI;
 
-namespace AppBoxDesign
+namespace AppBoxDesign;
+
+public sealed class LoginPage : View
 {
-    public sealed class LoginPage : View
-    {
 #if DEBUG
-        private readonly State<string> _userName = "Admin";
-        private readonly State<string> _password = "760wb";
+    private readonly State<string> _userName = "Admin";
+    private readonly State<string> _password = "760wb";
 #else
         private readonly State<string> _userName = "";
         private readonly State<string> _password = "";
 #endif
-        private readonly State<float> _inputSize = 20;
+    private readonly State<float> _inputSize = 20;
+    private readonly Image _bgImg;
 
-        public LoginPage()
-        {
-            Child = new Center
-            {
-                Child = new Card
-                {
-                    Width = 400, Height = 330,
-                    Elevation = 20,
-                    Child = BuildLoginForm()
-                }
-            };
-        }
+    public LoginPage()
+    {
+        var imgData = ResourceLoad.LoadBytes("Resources.Galaxy.webp");
+        _bgImg = Image.FromEncodedData(imgData)!;
 
-        private Widget BuildLoginForm()
+        Child = new Center
         {
-            return new Container
+            Child = new Card
             {
-                Padding = EdgeInsets.All(30),
-                Child = new Column(HorizontalAlignment.Center, 30)
+                Width = 400, Height = 330,
+                Elevation = 20,
+                Child = BuildLoginForm()
+            }
+        };
+    }
+
+    private Widget BuildLoginForm()
+    {
+        return new Container
+        {
+            Padding = EdgeInsets.All(30),
+            Child = new Column(HorizontalAlignment.Center, 30)
+            {
+                Children = new Widget[]
                 {
-                    Children = new Widget[]
+                    new Text("Welcome") { FontSize = 50 },
+                    new TextInput(_userName)
                     {
-                        new Text("Welcome") { FontSize = 50 },
-                        new TextInput(_userName)
-                        {
-                            HintText = "Account", FontSize = _inputSize,
-                            Prefix = new Icon(MaterialIcons.Person) { Size = _inputSize },
-                        },
-                        new TextInput(_password)
-                        {
-                            IsObscure = true, HintText = "Password", FontSize = _inputSize,
-                            Prefix = new Icon(MaterialIcons.Lock) { Size = _inputSize },
-                        },
-                        new Button("Login") { Width = 120, OnTap = e => OnLogin() }
-                    }
+                        HintText = "Account", FontSize = _inputSize,
+                        Prefix = new Icon(MaterialIcons.Person) { Size = _inputSize },
+                    },
+                    new TextInput(_password)
+                    {
+                        IsObscure = true, HintText = "Password", FontSize = _inputSize,
+                        Prefix = new Icon(MaterialIcons.Lock) { Size = _inputSize },
+                    },
+                    new Button("Login") { Width = 120, OnTap = e => OnLogin() }
                 }
-            };
-        }
+            }
+        };
+    }
 
-        private async void OnLogin()
+    private async void OnLogin()
+    {
+        try
         {
-            try
-            {
-                await DesignInitializer.TryInit();
-                await Channel.Login(_userName.Value, _password.Value);
-                CurrentNavigator!.Push("IDE");
-            }
-            catch (Exception ex)
-            {
-                Notification.Error($"登录错误: {ex.Message}");
-            }
+            await DesignInitializer.TryInit();
+            await Channel.Login(_userName.Value, _password.Value);
+            CurrentNavigator!.Push("IDE");
         }
+        catch (Exception ex)
+        {
+            Notification.Error($"登录错误: {ex.Message}");
+        }
+    }
+
+    public override void Paint(Canvas canvas, IDirtyArea? area = null)
+    {
+        canvas.DrawImage(_bgImg, Rect.FromLTWH(0, 0, W, H));
+        base.Paint(canvas, area);
     }
 }
---
+++ src/AppStudio/Resources/ResourceLoad.cs
@@ -0,0 +1,22 @@
+using System.IO;
+using System.Reflection;
+
+namespace AppBoxDesign.Resources;
+
+internal static class ResourceLoad
+{
+    private static readonly Assembly ResAssembly = typeof(ResourceLoad).Assembly;
+
+    public static Stream LoadStream(string res)
+    {
+        return ResAssembly.GetManifestResourceStream("AppBoxDesign." + res)!;
+    }
+
+    public static byte[] LoadBytes(string res)
+    {
+        using var stream = LoadStream(res);
+        var data = new byte[stream.Length];
+        stream.Read(data, 0, data.Length);
+        return data;
+    }
+}
