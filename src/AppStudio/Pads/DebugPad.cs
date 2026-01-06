7e6c8c6a9ed20845fcb906a737d7ced29f30d09f
EnjoyCode <enjoycode@icloud.com>
2026-01-06 10:19:53 +0800

    [AppStudio] 完善一下DebugPad的布局.

--- src/AppStudio/Pads/DebugPad.cs
+++ src/AppStudio/Pads/DebugPad.cs
@@ -10,26 +10,44 @@ internal sealed class DebugPad : View
 {
     public DebugPad(DesignStore store)
     {
-        Child = new Splitter()
+        _designStore = store;
+        // FillColor = new Color(0xFF3C3C3C);
+        Child = new Column()
         {
-            Panel1 = new EvaluateView(store),
-            Panel2 = new OutputView(),
+            Children =
+            [
+                BuildActionBar(),
+
+                new Conditional<int>(_activePad)
+                    .When(t => t == 0, () => new EvaluateView(store, _treeController))
+                    .When(t => t == 1, () => new OutputView())
+            ],
         };
     }
 
-    private class EvaluateView : View
-    {
-        public EvaluateView(DesignStore store)
-        {
-            _designStore = store;
+    private readonly DesignStore _designStore;
+    private readonly State<int> _activePad = 0; //当前的设计面板
+    private readonly State<string> _expression = ""; //需要计算值的表达式
+    private readonly TreeController<EvaluateResult> _treeController = new(); //计算结果树
 
-            Padding = EdgeInsets.All(5);
-            Child = new Column()
-            {
-                Spacing = 5,
-                Children =
-                [
-                    new Row()
+    private Widget BuildActionBar() => new Container()
+    {
+        /*FillColor = new Color(0xFF3C3C3C),*/ Height = 40,
+        Padding = EdgeInsets.Only(15, 5, 15, 5),
+        Child = new Row(VerticalAlignment.Middle, 10)
+        {
+            Children =
+            [
+                new ButtonGroup()
+                {
+                    Children =
+                    [
+                        new Button("Variables") { OnTap = _ => _activePad.Value = 0 },
+                        new Button("Output") { OnTap = _ => _activePad.Value = 1 },
+                    ]
+                },
+                new IfConditional(_activePad.ToComputed(p => p == 0),
+                    () => new Row()
                     {
                         Spacing = 5,
                         Children =
@@ -38,18 +56,53 @@ internal sealed class DebugPad : View
                             new Expanded(new TextInput(_expression)),
                             new Button("Evaluate") { OnTap = _ => OnEvaluate() }
                         ]
-                    },
-                    new TreeView<EvaluateResult>(_treeController, BuildTreeNode, n => n.Children ?? [])
-                    {
-                        LazyLoader = LazyLoadChildren
                     }
-                ]
+                ),
+            ]
+        }
+    };
+
+    private async void OnEvaluate()
+    {
+        if (string.IsNullOrEmpty(_expression.Value))
+        {
+            Notification.Error("Expression is empty");
+            return;
+        }
+
+        if (_designStore.ActiveDesigner == null ||
+            _designStore.ActiveDesigner is not IDebuggableCodeDesigner debuggable)
+        {
+            Notification.Error("The debuggable code editor is missing");
+            return;
+        }
+
+        try
+        {
+            var result = await debuggable.EvaluateExpression(_expression.Value);
+            //Notification.Info($"{result.Expression}[{result.Type}] = {result.Value}");
+            _treeController.DataSource = new List<EvaluateResult>() { result };
+        }
+        catch (Exception e)
+        {
+            Notification.Error(e.Message);
+        }
+    }
+
+    private class EvaluateView : View
+    {
+        public EvaluateView(DesignStore store, TreeController<EvaluateResult> treeController)
+        {
+            _designStore = store;
+
+            Padding = EdgeInsets.All(5);
+            Child = new TreeView<EvaluateResult>(treeController, BuildTreeNode, n => n.Children ?? [])
+            {
+                LazyLoader = LazyLoadChildren
             };
         }
 
         private readonly DesignStore _designStore;
-        private readonly State<string> _expression = "";
-        private readonly TreeController<EvaluateResult> _treeController = new();
 
         private static void BuildTreeNode(TreeNode<EvaluateResult> node)
         {
@@ -62,7 +115,7 @@ internal sealed class DebugPad : View
                     new Text(node.Data.Expression) { TextColor = Colors.Magenta },
                     new Text("="),
                     new Text($"{{{node.Data.Type}}}") { TextColor = Colors.Gray },
-                    new Text(node.Data.Value)
+                    new Text(node.Data.Value),
                 ]
             };
         }
@@ -78,33 +131,6 @@ internal sealed class DebugPad : View
 
             var children = await debuggable.ListChildren(node.Data.Name);
             node.Data.Children = children;
-        }
-
-        private async void OnEvaluate()
-        {
-            if (string.IsNullOrEmpty(_expression.Value))
-            {
-                Notification.Error("Expression is empty");
-                return;
-            }
-
-            if (_designStore.ActiveDesigner == null ||
-                _designStore.ActiveDesigner is not IDebuggableCodeDesigner debuggable)
-            {
-                Notification.Error("The debuggable code editor is missing");
-                return;
-            }
-
-            try
-            {
-                var result = await debuggable.EvaluateExpression(_expression.Value);
-                //Notification.Info($"{result.Expression}[{result.Type}] = {result.Value}");
-                _treeController.DataSource = new List<EvaluateResult>() { result };
-            }
-            catch (Exception e)
-            {
-                Notification.Error(e.Message);
-            }
         }
     }
 
