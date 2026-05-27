3b9b03dbc82a4a1f928a092e7b4fb5824e4811b6
EnjoyCode <enjoycode@icloud.com>
2026-05-27 14:49:29 +0800

    [Workfloat] 完善WorkflowParameter定义.

--- src/Core/Model/Workflow/WorkflowParameter.cs
+++ src/Core/Model/Workflow/WorkflowParameter.cs
@@ -16,51 +16,77 @@ public sealed class WorkflowParameter : IBinSerializable
         {
             if (_name != value)
             {
-                if (_originalName == null)
-                    _originalName = _name;
+                _originalName ??= _name;
                 _name = value;
             }
         }
     }
 
-    public WorkflowParameterType Type { get; set; }
+    public ValueType Type { get; set; }
+
+    public ParameterFlags Flags { get; set; }
+
+    /// <summary>
+    /// 是否工作流局部变量，非局部变量在启动实例时由外部输入且必需
+    /// </summary>
+    public bool IsLocalVariable => (Flags & ParameterFlags.LocalVariable) == ParameterFlags.LocalVariable;
+
+    public string? Remark { get; set; }
 
     public void AcceptChanges()
     {
         _originalName = null;
     }
 
+    #region ====Serialization====
+
     public void WriteTo<TWriter>(ref TWriter bs) where TWriter : struct, IOutputStream
     {
-        bs.WriteFieldId(1);
+        bs.WriteByte((byte)Type);
+        bs.WriteByte((byte)Flags);
         bs.WriteString(Name);
 
-        bs.WriteFieldId(2);
-        bs.WriteByte((byte)Type);
+        if (!string.IsNullOrEmpty(Remark))
+        {
+            bs.WriteFieldId(1);
+            bs.WriteString(Remark);
+        }
 
         bs.WriteFieldEnd();
     }
 
     public void ReadFrom<TReader>(ref TReader bs) where TReader : struct, IInputStream
     {
+        Type = (ValueType)bs.ReadByte();
+        Flags = (ParameterFlags)bs.ReadByte();
+        Name = bs.ReadString()!;
+
         int propIndex;
         do
         {
             propIndex = bs.ReadFieldId();
             switch (propIndex)
             {
-                case 1: Name = bs.ReadString()!; break;
-                case 2: Type = (WorkflowParameterType)bs.ReadByte(); break;
+                case 1: Remark = bs.ReadString()!; break;
                 case 0: break;
                 default: throw SerializationException.ReadUnknownField(nameof(StartNode), propIndex);
             }
         } while (propIndex != 0);
     }
-}
 
-public enum WorkflowParameterType : byte
-{
-    Entity = 0,
-    String = 1,
-    Number = 2,
+    #endregion
+
+    public enum ValueType : byte
+    {
+        Entity = 0,
+        String = 1,
+        Number = 2,
+    }
+
+    [Flags]
+    public enum ParameterFlags : byte
+    {
+        None = 0,
+        LocalVariable = 1,
+    }
 }
