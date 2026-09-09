a0b0ab9abf0de50994c7fec10d23f3ef90e3a1a0
EnjoyCode <enjoycode@icloud.com>
2026-09-09 14:17:35 +0800

    upgrade PixUI.

--- src/Store.Initiator/Resources/Views/MetricsDashboard.cs
+++ src/Store.Initiator/Resources/Views/MetricsDashboard.cs
@@ -5,28 +5,32 @@ public sealed class MetricsDashboard : View
     public MetricsDashboard()
     {
         FillColor = Colors.Black;
-        Child = new Column
-        {
-            Children =
-            [
-                new Row
-                {
-                    Children =
-                    [
-                        BuildCard(new CpuUsage()).WrapByExpanded(),
-                        BuildCard(new MemUsage()).WrapByExpanded(),
-                    ]
-                }.WrapByExpanded() ,
-                new Row
-                {
-                    Children =
-                    [
-                        BuildCard(new ThreadPoolCount()).WrapByExpanded(),
-                        BuildCard(new GcCollections()).WrapByExpanded()
-                    ]
-                }.WrapByExpanded()
-            ]
-        };
+        Child = new Column().WithChildren([
+            BuildCmdBar(),
+            new Row().WithChildren([
+                BuildCard(new CpuUsage()).WrapByExpanded(),
+                BuildCard(new MemUsage()).WrapByExpanded()
+            ]).WrapByExpanded(),
+            new Row().WithChildren([
+                BuildCard(new ThreadPoolCount()).WrapByExpanded(),
+                BuildCard(new GcCollections()).WrapByExpanded()
+            ]).WrapByExpanded()
+        ]);
+    }
+
+    private Widget BuildCmdBar()
+    {
+        var row = new Row() { Spacing = 5 }.WithChildren([
+            new Text("Start:") { TextColor = Colors.White},
+            new DatePicker(DateTime.Now).WithWidth(120),
+            new Text("End:") { TextColor = Colors.White},
+            new DatePicker(DateTime.Now).WithWidth(120),
+            new Expanded(),
+            new Button(icon: MaterialIcons.Refresh)
+        ]);
+        var container = new Container() { Padding = EdgeInsets.All(5), Height = 44 }
+            .WithChild(row);
+        return BuildCard(container);
     }
 
     private Card BuildCard(Widget child) => new Card
